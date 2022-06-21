799b111bf8c49ba606bc592ce6a922768ff4248e
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2022-06-21 09:55:02 +0500

    routes add

---
+++ lib/routes/routes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyRoutes {
+  static final MyRoutes _instanse = MyRoutes.init();
+  static MyRoutes get instanse => MyRoutes._instanse;
+  MyRoutes.init();
+
+  Route? onGenerate(RouteSettings s) {
+    var args = s.arguments;
+    switch (s.name) {
+      case '/auth':
+        // return MaterialPageRoute(builder: (_) => ());
+      
+    }
+  }
+}
